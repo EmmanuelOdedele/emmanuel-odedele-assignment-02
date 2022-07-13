e4e0e1989038319c2595f074be4c39b2eacf0424
Emmanuel Odedele <69359947+EmmanuelOdedele@users.noreply.github.com>
2022-07-13 18:15:44 +0000

    Check Challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,17 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum = 0;
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+        sum += (arrayOfNumbers[i]);
+    }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+console.log(sumOfNumbers([2, 3, 4, 5, 6]))
+
 
 /**
  * Challenge - 2
@@ -27,10 +34,14 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.countEvenNumbers = countEvenNumbers;
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+const countEvenNumbers = arr.filter(number => {
+    return number % 2 == 0;
+});
+
+console.log(countEvenNumbers)
 
 /**
  * Challenge - 3
